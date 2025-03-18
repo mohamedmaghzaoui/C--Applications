b9b8b7153ce803b623b4d5ff4d3ab77ba16a53f1
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2025-03-18 16:51:41 +0100

    calculator

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+class Program
+{
+    static void Main(){
+        double result;
+        double firstValue;
+        double secondValue;
+        string operation;
+        Console.WriteLine("first value : ");
+        while (true)
+        {
+            
+            try
+            {
+                firstValue=Convert.ToDouble(Console.ReadLine());      
+                break;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("first value : ");
+            }
+            
+        }
+        Console.WriteLine("second value : ");
+        while (true)
+        {
+           
+            try
+            {
+                secondValue=Convert.ToDouble(Console.ReadLine());  
+                break;   
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("second value : ");
+            }
+            
+        }
+        Console.WriteLine("operation : ");
+        while (true)
+        {
+                 
+            operation=Console.ReadLine();   
+            if (operation =="+" || operation =="-" || operation =="*" || operation =="/" )
+            {
+                break;
+            }else
+            {
+                Console.WriteLine("operation : ");
+            }
+        }
+
+   
+        switch (operation)
+        {
+            case "+":
+            result=firstValue+secondValue;
+            break;
+            case "-":
+            result=firstValue-secondValue;
+            break;
+            case "*":
+            result=firstValue*secondValue;
+            break;
+            case "/":
+            result=firstValue/secondValue;
+            break;
+            
+            default:
+                result=0;
+                break;
+        }
+        Console.WriteLine("result = "+result);
+
+
+    }
+    
+}
